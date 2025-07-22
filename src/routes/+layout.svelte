6db91fd2c153b5c2e8bf3865fabae909e78b9b78
CCleanerShot <31040856+CCleanerShot@users.noreply.github.com>
2025-07-22 13:19:37 -0500

    changed site to be layout-based + downloaded mongodb package + set dependencies to static version

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,3 +5,19 @@
 </script>
 
 {@render children()}
+<div class="absolute border-2 bottom-1 left-1/2 transition-all -translate-x-1/2" id="table-of-contents">
+	<a href="/account">Account</a>
+	<a href="/meetups">Meetups</a>
+</div>
+
+<style>
+	a {
+		font-weight: bold;
+		padding: 0.2rem;
+	}
+
+	a:hover {
+		background-color: black;
+		color: white;
+	}
+</style>
---
+++ src/routes/account/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import google from '$lib/images/google.png';
+</script>
+
+
+<button class="border-2 rounded-md p-1 flex gap-8 items-center justify-between">
+    <img src={google} alt="google icon" width="20" />
+    <span>Sign Up With Google</span>
+</button>
